930c90b6eec321a1541aa1bc787e22d9fb8a4ce7
PeterNguyen <peter.nguyen@vmodev.com>
2020-06-23 16:44:21 +0700

    add components

---
+++ src/components/ItemList.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import { itemsFetchData} from '../actions/items';
+
+class ItemList extends Component {
+    componentDidMount() {
+        this.props.fetchData('http://599167402df2f40011e4929a.mockapi.io/items');
+    }
+    render() {
+        if (this.props.hasError) {
+            return <p> Sorry! There was an error loading the items </p>;
+        }
+        if (this.props.isLoading) {
+            return <p>Loading...</p>;
+        }
+
+        return (
+            <ul>
+                {this.props.items.map(item => (
+                    <li key={item.id} >{item.label}</li>
+                ))}
+            </ul>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        items: state.items,
+        hasError: state.itemsHasError,
+        isLoading: state.itemsIsLoading
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        fetchData: url => dispatch(itemsFetchData(url))
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ItemList);
